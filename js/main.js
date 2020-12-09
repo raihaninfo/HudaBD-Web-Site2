dc058a19372f443fd209ed66a14bee72af6f4911
Md Abu Raihan <raihanmahmudi35@gmail.com>
2020-12-09 15:31:56 +0600

    sline section slider added

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        autoplay: true,
+        autoplayHoverPause: true,
+        loop: true,
+        autoplayTimeout: 3000,
+    });
+  });
